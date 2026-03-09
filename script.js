f9b4e15984798fa9a15dbe5ecc313d00a8da6990
Anamul Islam <68754553+anamul24@users.noreply.github.com>
2026-03-10 03:26:00 +0600

    bug fixed

--- script.js
+++ script.js
@@ -23,6 +23,25 @@ function displayIssues(issues){
         card.className=` relative bg-white p-4 pt-8 rounded shadow border-t-4 ${border} cursor-pointer hover:shadow-lg `
         let statusIcon= issue.status.toLowerCase() === "open" ? "image/Open-Status.png" : "image/Closed-Status.png";
 
+        
+        let labelsHTML="";
+        issue.labels.forEach(label =>{
+            let color = "bg-gray-200 text-gray-700";
+            if(label ==="bug"){
+                color="bg-red-100 text-red-600";
+            }
+            else if(label === "enhancement"){
+                color= "bg-blue-100 text-blue-600";
+            }
+            else if(label === "documentation"){
+                color ="bg-purple-100 text-purple-600";
+            }
+            else if(label === "good first issue"){
+                color="bg-green-100 text-green-600";
+            }
+            labelsHTML+=`<span class="px-2 py-1 rounded ${color}">${label}</span>`;
+        });
+
         card.innerHTML=`<div class="flex justify-between mb-2">
                         <img src="${statusIcon}" class="absolute top-2 left-2 w-5 h-5">
                         <span class="absolute top-2 right-2 text-xs px-2 py-1 rounded ${priorityClass}">${issue.priority}</span>
@@ -32,20 +51,18 @@ function displayIssues(issues){
 
                         <p class="text-gray-500 text-xs mb-3 line-clamp-2">${issue.description}</p>
 
-                        
-                        <div class="flex gap-2 mb-3 text-xs">
-                        <span class="bg-red-100 text-red-500 px-2 py-1 rounded">BUG</span>
-                        <span class="bg-yellow-100 text-yellow-600 px-2 py-1 rounded">HELP WANTED</span>
-                        </div>
+                        <div class="flex gap-2 mb-3 text-xs flex-wrap">${labelsHTML}</div>
+
 
                         <div class="text-xs text-gray-400">
-                        #${issue.id} by ${issue.author}<br> ${issue.createdAt}
+                        #${issue.id} by ${issue.author}<br> ${new Date(issue.createdAt).toLocaleDateString()}
                         </div>
                         `;
         card.onclick = () =>openModal(issue.id)
         container.appendChild(card)
         
     });
+    document.getElementById("modalLabels").innerHTML=labelsHTML;
 };
 async function setActive(type) {
     document.querySelectorAll(".tab-btn").forEach(btn=> btn.classList.remove("bg-purple-600", "text-white"))
@@ -74,6 +91,26 @@ async function openModal(id) {
     const data =await res.json()
     const issue = data.data
 
+    
+        let labelsHTML="";
+        issue.labels.forEach(label =>{
+            let color = "bg-gray-200 text-gray-700";
+            if(label ==="bug"){
+                color="bg-red-100 text-red-600";
+            }
+            else if(label === "enhancement"){
+                color= "bg-blue-100 text-blue-600";
+            }
+            else if(label === "documentation"){
+                color ="bg-purple-100 text-purple-600";
+            }
+            else if(label === "good first issue"){
+                color="bg-green-100 text-green-600";
+            }
+            labelsHTML+=`<span class="px-2 py-1 rounded ${color}">${label}</span>`;
+        });
+
+    document.getElementById("modalLabels").innerHTML=labelsHTML;
     document.getElementById("modalTitle").innerText= issue.title
     document.getElementById("modalDescription").innerText= issue.description
     document.getElementById("modalAuthor").innerText= `Opened by ${issue.author} - ${new Date(issue.createdAt).toLocaleDateString()}`
